465bca816f81de95e44a58808d653524123a79d8
郭鹏 <532957426@qq.com>
2015-09-28 00:13:59 +0800

    完成页面的爬取，等待进行html的解析

--- scraping.py
+++ scraping.py
@@ -38,7 +38,44 @@ button = driver.find_element_by_class_name('btn_search')
 button.click()
 time.sleep(2)
 
-f = open('result.html', 'w')
+# 翻滚页面至加载完毕，两种不同的方法
+flight_subscribe = driver.find_element_by_class_name('subenv')
+for x in xrange(5):
+    driver.execute_script("return arguments[0].scrollIntoView();", flight_subscribe)
+    time.sleep(1)
+# page_detail = driver.find_element_by_class_name('e_fly_lst')
+# detail_list = page_detail.find_elements_by_xpath('div')
+# detail_num = len(detail_list)
+# print 'detail number : ', detail_num
+#
+# flight_subscribe = driver.find_element_by_class_name('subenv')
+# driver.execute_script("return arguments[0].scrollIntoView();", flight_subscribe)
+# time.sleep(1)
+# detail_list = page_detail.find_elements_by_xpath('div')
+#
+# while len(detail_list) != detail_num:
+#     detail_num = len(detail_list)
+#     driver.execute_script("return arguments[0].scrollIntoView();", flight_subscribe)
+#     time.sleep(1)
+#     detail_list = page_detail.find_elements_by_xpath('div')
+#     print 'detail number : ', len(detail_list)
+
+# 得到所有的结果页面
+print 'store the ' + str(1) + ' page'
+f = open('result_1.html', 'w')
 f.write(driver.page_source.encode('utf-8'))
 f.close()
+
+page_turning = driver.find_element_by_id('detailPage')
+if page_turning.is_displayed():
+    pages = page_turning.find_elements_by_xpath('div[@id=\'hdivPager\']/a')
+    for x in xrange(len(pages) - 1):
+        pages[-1].click()
+        for y in xrange(5):
+            driver.execute_script("return arguments[0].scrollIntoView();", flight_subscribe)
+            time.sleep(1)
+        print 'store the ' + str(x + 2) + ' page'
+        f = open('result_' + str(x + 2) + '.html', 'w')
+        f.write(driver.page_source.encode('utf-8'))
+        f.close()
 driver.quit()
